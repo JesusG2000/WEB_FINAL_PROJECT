a8c777b0e33f78177dc95c47f657d1d7f0b44b21
Ivanko Verigo <verigoivan00@mail.ru>
2020-01-31 10:22:06 +0300

    add sub to vacancy

---
+++ src/main/java/servlet/no_view/SubToVacancy.java
@@ -0,0 +1,36 @@
+package servlet.no_view;
+
+import bean.User;
+import bean.dto.VacResponded;
+import dao.Dao;
+import dao.impl.JdbcVacRespondedDao;
+import servlet.MainServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/subVacancy")
+public class SubToVacancy extends MainServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (isLogin(req) != null) {
+            Dao<VacResponded> vacRespondedDao = new JdbcVacRespondedDao();
+            int vacId = Integer.parseInt(req.getParameter("vacId"));
+            int userId = Integer.parseInt(req.getParameter("userId"));
+
+            vacRespondedDao.create(new VacResponded(userId, vacId));
+            resp.sendRedirect("/home");
+        } else {
+            resp.sendRedirect("/login");
+        }
+
+    }
+
+    @Override
+    public User isLogin(HttpServletRequest request) {
+        return checkLogin(request);
+    }
+}
