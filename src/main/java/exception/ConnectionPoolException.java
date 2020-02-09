601213b5910d0809fc0810f85e829eca90bbd6eb
Ivanko Verigo <verigoivan00@mail.ru>
2020-02-09 15:58:42 +0300

    add exception

---
+++ src/main/java/exception/ConnectionPoolException.java
@@ -0,0 +1,21 @@
+package exception;
+
+public class ConnectionPoolException extends Exception {
+
+
+    public ConnectionPoolException() {
+        super();
+    }
+
+    public ConnectionPoolException(String message) {
+        super(message);
+    }
+
+    public ConnectionPoolException(Exception e) {
+        super(e);
+    }
+
+    public ConnectionPoolException(String message, Exception e) {
+        super(message, e);
+    }
+}
